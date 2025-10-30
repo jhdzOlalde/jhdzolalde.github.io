f2339a75eb0f2557b8fb2659098081e4799c191b
jhdzOlalde <jhdzolalde@gmail.com>
2025-10-30 11:14:22 -0600

    .

--- Sentencias Y practicas de PHP/P2.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $nombre = "Francisco";
-    $numero1= 1;
-    $numero2 = 2;
-    $total = $numero1 + $numero2;
-    //echo 'Suma es,'. $total; 
-    echo gettype($nombre);
-
-?>
--- Sentencias Y practicas de PHP/P5.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $alexio= array('nombre'=> 'Alexio', 'edad' => '22 años', 'Colonia' => 'San agustin');
-    echo $alexio['Colonia'];
-?>
